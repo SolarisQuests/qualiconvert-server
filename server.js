8cd2508f460d464e00dba958aa8107edd50bfe13
SolarisQuests <160103412+SolarisQuests@users.noreply.github.com>
2024-12-09 16:50:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const mailgun = require('mailgun-js');
 require('dotenv').config();
 
 const app = express();
