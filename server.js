5cf3ace8480a583db6f3568a94fc3b587a0b28bb
SolarisQuests <160103412+SolarisQuests@users.noreply.github.com>
2024-12-26 19:59:47 +0530

    pdf attached

--- server.js
+++ server.js
@@ -194,10 +194,10 @@ app.post('/api/submit-form', async (req, res) => {
     // Send email using Mailgun
     const data = {
       from: 'Qualiconvert <noreply@qualiconvert.com>',
-      to : 'sriram@legaciestechno.com',
-      //to: formData.email,
-      //cc: 'sheldon@auxoinnovation.com', 
-     // bcc:'noreply@auxoinnovations.com,anthony@auxoinnovations.com', 
+     //  to : 'sriram@legaciestechno.com',
+     to: formData.email,
+      cc: 'sheldon@auxoinnovation.com', 
+       bcc:'noreply@auxoinnovations.com,anthony@auxoinnovations.com', 
       subject: 'New Onboarding Form Submission',
       text: `Thank you for submitting your onboarding form. Here are the details you provided:\n\n${emailContent}`,
       attachment: pdfPath, // Attached the generated PDF
